7ed1cd87096c07091ede117fea70083b50b09f9b
“Ryota <kurokwa.ryota.1124@gmail.com>
2023-08-25 13:40:32 +0900

    アカウントの作成、保存、ログイン機能の実装

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:hackathon/pages/profile_page.dart';
+import 'package:hackathon/pages/signup_page.dart';
+import 'package:hackathon/share/authenticator.dart';
 
 class loginPage extends StatelessWidget {
-  const loginPage({super.key});
+  loginPage({super.key});
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(top: 200),
+          ),
+          const Center(
+            child: Text(
+              'Hello World',
+              style: TextStyle(fontSize: 50),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 62, vertical: 52),
+            child: TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(hintText: 'email'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 62, vertical: 36),
+            child: TextField(
+              controller: _passController,
+              decoration: const InputDecoration(hintText: 'password'),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 80),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => SignupPage(),
+                ),
+              );
+            },
+            child: const Text(
+              'make your account',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              var _result = await authenticator.emailSignIn(
+                  email: _emailController.text, password: _passController.text);
+              if (_result == true) {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => ProfilePage(),
+                  ),
+                );
+              }
+            },
+            child: Text(
+              'login',
+              style: TextStyle(
+                fontSize: 40,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,1 +1,140 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hackathon/pages/search_page.dart';
 
+class ProfilePage extends StatelessWidget {
+  ProfilePage({super.key});
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _oldController = TextEditingController();
+  final TextEditingController _matchingGenderController =
+      TextEditingController();
+  final TextEditingController _commentontCroller = TextEditingController();
+  final TextEditingController _genderController = TextEditingController();
+  final db = FirebaseFirestore.instance;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: 600,
+          width: 300,
+          color: Colors.white,
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 40),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 80, right: 80),
+                child: TextField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(
+                    label: Text('名前'),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 40),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 80, right: 80),
+                child: TextField(
+                  controller: _oldController,
+                  decoration: const InputDecoration(
+                    label: Text('年齢'),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 40),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 80, right: 80),
+                child: TextField(
+                  controller: _genderController,
+                  decoration: const InputDecoration(
+                    label: Text('性別'),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 40),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 80, right: 80),
+                child: TextField(
+                  controller: _commentontCroller,
+                  decoration: const InputDecoration(
+                    label: Text('自己紹介'),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 40),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 80, right: 80),
+                child: TextField(
+                  controller: _matchingGenderController,
+                  decoration: const InputDecoration(
+                    label: Text('対象性別'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 40),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  setprofile(
+                      _nameController.text,
+                      _oldController.text,
+                      _genderController.text,
+                      _matchingGenderController.text,
+                      _commentontCroller.text);
+                  await Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => searchPage(),
+                    ),
+                  );
+                },
+                child: const Text('完了'),
+              )
+            ],
+          ),
+        ),
+      ),
+      // backgroundColor: Colors.orange,
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     Navigator.of(context).push(
+      //       MaterialPageRoute(
+      //         builder: (context) => matchingPage(),
+      //       ),
+      //     );
+      //   },
+      //   child: const Text("Next"),
+      // ),
+    );
+  }
+
+  Future<void> setprofile(String name, String old, String gender,
+      String matchingGender, String comment) async {
+    await db
+        .collection('users')
+        .doc(
+          FirebaseAuth.instance.currentUser!.uid.toString(),
+        )
+        .set({
+      'name': name,
+      'old': old,
+      'gender': gender,
+      'matchingGender': matchingGender,
+      'comment': comment,
+      'email': FirebaseAuth.instance.currentUser!.email.toString(),
+    });
+  }
+}
