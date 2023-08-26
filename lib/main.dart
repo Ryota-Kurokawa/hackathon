4bc50c540da6a4243f91dd2ffa632a5d57aca574
“Ryota <kurokwa.ryota.1124@gmail.com>
2023-08-26 16:49:41 +0900

    Configを追加

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,21 @@ class Start extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: loginPage(),
+      home: Config(),
     );
+  }
+}
+
+class Config extends StatefulWidget {
+  const Config({super.key});
+
+  @override
+  State<Config> createState() => _ConfigState();
+}
+
+class _ConfigState extends State<Config> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
