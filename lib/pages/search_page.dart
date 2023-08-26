bae7c447d2f0e6a466c07b89d2e9396d29777911
ryota <119117263+Ryota-Kurokawa@users.noreply.github.com>
2023-08-27 03:50:48 +0900

    直前のおおまかなUI設計 (#18)
    Co-authored-by: “Ryota <kurokwa.ryota.1124@gmail.com>

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -4,7 +4,9 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hackathon/models/restaurant.dart';
+import 'package:hackathon/pages/good_stores_page.dart';
 import 'package:hackathon/pages/profile_edit_page.dart';
+import 'package:hackathon/pages/success_matching_page.dart';
 import 'package:hackathon/widgets/restaurant_card2.dart';
 import 'package:http/http.dart' as http;
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -22,11 +24,26 @@ class searchPage extends HookConsumerWidget {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         leading: IconButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => profileEditPage()));
-            },
-            icon: Icon(Icons.account_balance_outlined)),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => profileEditPage(),
+              ),
+            );
+          },
+          icon: Icon(Icons.account_balance_outlined),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => goodStoresPage(),
+                  ),
+                );
+              },
+              icon: Icon(Icons.message_outlined))
+        ],
         backgroundColor: Color.fromARGB(255, 215, 199, 47),
       ),
       body: Column(
@@ -38,10 +55,6 @@ class searchPage extends HookConsumerWidget {
             padding: const EdgeInsets.only(left: 48, right: 48),
             child: TextField(
               onSubmitted: (String value) async {
-                // final result = await searchRestaurant(value);
-                // setState(() => restaurants = results); //riverpodで状態管理を試してみる
-                debugPrint("Search!");
-                debugPrint(value);
                 final result = await searchRestaurant(value);
                 restaurants.value = result!;
               },
@@ -62,6 +75,16 @@ class searchPage extends HookConsumerWidget {
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => successMatchingPage(),
+            ),
+          );
+        },
+        child: Text('matching成功'),
+      ),
     );
   }
 
@@ -69,14 +92,12 @@ class searchPage extends HookConsumerWidget {
     final String KEY = dotenv.env['HOT_PEPPER_KEY'] ?? '';
 
     var url = Uri.https('webservice.recruit.co.jp', 'hotpepper/gourmet/v1/',
-        {'key': KEY, 'keyword': keyword, 'format': 'json'});
+        {'key': KEY, 'keyword': keyword, 'count': '20', 'format': 'json'});
     // var response = await http.post(
     //   url,
     //   body: {'key': KEY, 'keyword': keyword, 'format': 'json'},
     // );
     final response = await http.get(url);
-    debugPrint(response.body.toString());
-
     if (response.statusCode == 200) {
       final list = jsonDecode(response.body);
       final restaurants = List<Restaurant>.from(
